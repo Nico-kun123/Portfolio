0cb6b9a3597814a0bf7b6814f95c34da3d13f907
Nicolay Kudryavtsev <nicolay.kudryavtsev@gmail.com>
2024-06-16 20:20:07 +0700

    Добавил vite-plugin-purgecss для удаления ненужных CSS

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import pluginPurgeCss from '@mojojoejo/vite-plugin-purgecss'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [pluginPurgeCss(), vue()],
   optimizeDeps: {
     include: ['vue']
   },
@@ -13,6 +15,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
     }
   }
 })
