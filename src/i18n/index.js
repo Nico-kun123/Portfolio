64bccaef6e690aa85f445f2165e3c2004fcad0ad
Nicolay Kudryavtsev <nicolay.kudryavtsev@gmail.com>
2024-06-21 13:38:31 +0700

    Add minor changes in messages

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,28 +5,28 @@ import english from '../locales/en.json'
 import russian from '../locales/ru.json'
 
 const i18n = createI18n({
-  // Устанавливаем локаль по умолчанию
-  locale: navigator.language,
-  // locale: 'ru',
-  // locale: 'en',
-
   // Устанавливаем локаль, если не найдена установленная локаль по умолчанию
   fallbackLocale: 'en',
 
-  // Устанавливаем флаг, чтобы не выдавало ошибку, если локаль не найдена
-  sync: true,
-
-  // Отключить предупреждения о недостающих ключах
-  missingWarn: false,
-
   // Отключить предупреждения о недостающих переводах
   legacy: false,
+
+  // Устанавливаем локаль по умолчанию
+  locale: navigator.language,
+  // Locale: 'ru',
+  // Locale: 'en',
 
   // Переводы на языки: русский и английский
   messages: {
     en: english,
     ru: russian
-  }
+  },
+
+  // Отключить предупреждения о недостающих ключах
+  missingWarn: false,
+
+  // Устанавливаем флаг, чтобы не выдавало ошибку, если локаль не найдена
+  sync: true
 })
 
 export default i18n
