f67de0d5eb74ece06df91751b96de641913c9c7e
Nicolay Kudryavtsev <nicolay.kudryavtsev@gmail.com>
2025-04-05 16:02:05 +0700

    Fixed deps

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
-import pluginPurgeCss from '@mojojoejo/vite-plugin-purgecss'
+//! Minify html in production
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+
+//! Для Vite с версией 5+
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [pluginPurgeCss(), vue()],
-  optimizeDeps: {
-    include: ['vue']
-  },
   base: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
         chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
+  optimizeDeps: { include: ['vue'] },
+  plugins: [vue(), ViteMinifyPlugin({}), pluginPurgeCss({ variables: true })],
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
 })
